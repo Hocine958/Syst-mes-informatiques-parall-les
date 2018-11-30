8221e61f8f32bc30535f7fee2ed093a1e5d42d49
Vincent Brouillard <vinpas45@gmail.com>
2018-11-30 13:05:49 -0500

    ajout des résultats de la partie 3

---
+++ tp3/inf8601-lab3-2.1.5/cluster/heatsim-16.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#These commands set up the Grid Environment for your job:
+#PBS -N job-heatsim-16
+#PBS -q recherche
+#PBS -j oe
+#PBS -l nodes=16:ppn=8
+
+source $PBS_O_WORKDIR/lib.sh
+
+do_run 128 128 1 "$ITERATIONS" "$IMAGES"
+do_run 128 16 8 "$ITERATIONS" "$IMAGES"
+
